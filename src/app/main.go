bd44faf387d829eb475b4c94e9e79911981ab49f
superbobby2000 <coudercleo10@gmail.com>
2024-06-21 13:44:48 -0400

    initialize database in main context

--- src/app/main.go
+++ src/app/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
 	"github.com/pc-power-api/src/controller"
 	"github.com/pc-power-api/src/controller/middleware"
+	"github.com/pc-power-api/src/infra/entity"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"log"
 	"os"
 )
@@ -16,5 +20,37 @@ func main() {
 
 	controller.NewDevicesHandler(r)
 
+	db := connectDatabase()
+	err := db.AutoMigrate(&entity.User{})
+	err = db.AutoMigrate(&entity.Device{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	log.Fatal(r.Run(":" + port))
+}
+
+func connectDatabase() *gorm.DB {
+	if os.Getenv("DBTYPE") == "mysql" {
+		username := os.Getenv("DBUSER")
+		password := os.Getenv("DBPASS")
+		host := os.Getenv("DBHOST")
+		port := os.Getenv("DBPORT")
+		dbname := os.Getenv("DBNAME")
+		dsn := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbname + "?charset=utf8mb4&parseTime=True&loc=Local"
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		return db
+	} else if os.Getenv("DBTYPE") == "sqlite" {
+		db, err := gorm.Open(sqlite.Open("db/gorm.db"), &gorm.Config{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		return db
+	} else {
+		log.Fatal("Database type not supported")
+		return nil
+	}
 }
