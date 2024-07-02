1c249c2ae8301ce565fe29b518fe488e75eaae0c
superbobby2000 <coudercleo10@gmail.com>
2024-07-02 13:57:56 -0400

    Move user's devices crud operations to the user controller

--- src/controller/devices.go
+++ src/controller/devices.go
@@ -4,15 +4,11 @@ import (
 	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 	"github.com/go-errors/errors"
-	"github.com/google/uuid"
 	"github.com/pc-power-api/src/api"
 	"github.com/pc-power-api/src/controller/gateway"
 	"github.com/pc-power-api/src/controller/middleware"
 	"github.com/pc-power-api/src/exceptions"
-	"github.com/pc-power-api/src/infra/entity"
 	"github.com/pc-power-api/src/infra/repo"
-	"github.com/pc-power-api/src/pubsub"
-	"github.com/pc-power-api/src/util"
 	"net/http"
 )
 
@@ -39,11 +35,6 @@ func NewDevicesHandler(e *gin.Engine, jwtMiddleware *jwt.GinJWTMiddleware, devic
 		group.GET("/gateway", handler.gateway)
 		group.POST("/power-switch", jwtMiddleware.MiddlewareFunc(), handler.pressPowerSwitch)
 		group.POST("/reset-switch", jwtMiddleware.MiddlewareFunc(), handler.pressResetSwitch)
-		group.GET("/", jwtMiddleware.MiddlewareFunc(), handler.getDevices)
-		group.POST("/", jwtMiddleware.MiddlewareFunc(), handler.createDevice)
-		group.GET("/:"+IdPathParam, jwtMiddleware.MiddlewareFunc(), handler.getDevice)
-		group.PUT("/:"+IdPathParam, jwtMiddleware.MiddlewareFunc(), handler.updateDevice)
-		group.DELETE("/:"+IdPathParam, jwtMiddleware.MiddlewareFunc(), handler.deleteDevice)
 	}
 }
 
@@ -125,167 +116,5 @@ func (h *DevicesHandler) pressResetSwitch(c *gin.Context) {
 		c.Error(errors.New(DeviceNotConnectedError))
 		return
 	}
-	c.Status(http.StatusNoContent)
-}
-
-func (h *DevicesHandler) getDevices(c *gin.Context) {
-	userId := middleware.GetUserIdFromContext(c)
-	user, aerr := h.userRepo.GetById(userId)
-	if aerr != nil {
-		c.Error(aerr)
-		return
-	}
-
-	devicesInfoList := api.DeviceInfoList{
-		OnlineDevices:  make([]api.DeviceInfo, 0),
-		OfflineDevices: make([]api.DeviceInfo, 0),
-	}
-	for _, device := range user.Devices {
-		if conn, ok := gateway.ConnectedDevices[device.ID]; ok {
-			devicesInfoList.OnlineDevices = append(devicesInfoList.OnlineDevices, api.DeviceInfo{
-				ID:     device.ID,
-				Name:   device.Name,
-				Code:   device.Code,
-				Secret: device.Secret,
-				Status: conn.GetStatus(),
-				Online: true,
-			})
-		} else {
-			devicesInfoList.OfflineDevices = append(devicesInfoList.OfflineDevices, api.DeviceInfo{
-				ID:     device.ID,
-				Name:   device.Name,
-				Code:   device.Code,
-				Secret: device.Secret,
-				Status: 0,
-				Online: false,
-			})
-		}
-	}
-
-	c.JSON(http.StatusOK, devicesInfoList)
-}
-
-func (h *DevicesHandler) getDevice(c *gin.Context) {
-	deviceId := c.Param(IdPathParam)
-	device, aerr := h.deviceRepo.GetById(deviceId)
-	if aerr != nil {
-		c.Error(aerr)
-		return
-	}
-
-	ownerId := middleware.GetUserIdFromContext(c)
-	if ownerId != device.UserID {
-		c.Error(errors.New(UserDoesNotOwnDevice))
-		return
-	}
-
-	status := 0
-	online := false
-	if conn, ok := gateway.ConnectedDevices[device.ID]; ok {
-		status = conn.GetStatus()
-		online = true
-	}
-	c.JSON(http.StatusOK, api.DeviceInfo{
-		ID:     device.ID,
-		Name:   device.Name,
-		Code:   device.Code,
-		Secret: device.Secret,
-		Status: status,
-		Online: online,
-	})
-}
-
-func (h *DevicesHandler) createDevice(c *gin.Context) {
-	var deviceInfo *api.DeviceCreateInfo
-	err := c.ShouldBind(&deviceInfo)
-	if err != nil {
-		c.Error(errors.New(err))
-		return
-	}
-
-	ownerId := middleware.GetUserIdFromContext(c)
-	deviceUuid := uuid.New()
-	deviceCode := util.GenerateRandomString(DeviceCodeLength)
-	deviceSecret := util.GenerateRandomString(DeviceSecretLength)
-
-	var device = entity.Device{
-		ID:     deviceUuid.String(),
-		Name:   deviceInfo.Name,
-		Code:   deviceCode,
-		Secret: deviceSecret,
-		UserID: ownerId,
-	}
-
-	aerr := h.deviceRepo.Create(&device)
-	if aerr != nil {
-		c.Error(aerr)
-		return
-	}
-	pubsub.Publish(ownerId, device)
-
-	c.JSON(http.StatusOK, api.DeviceInfo{
-		ID:     device.ID,
-		Name:   device.Name,
-		Code:   device.Code,
-		Secret: device.Secret,
-	})
-}
-
-func (h *DevicesHandler) updateDevice(c *gin.Context) {
-	deviceId := c.Param(IdPathParam)
-	device, aerr := h.deviceRepo.GetById(deviceId)
-	if aerr != nil {
-		c.Error(aerr)
-		return
-	}
-
-	ownerId := middleware.GetUserIdFromContext(c)
-	if ownerId != device.UserID {
-		c.Error(errors.New(UserDoesNotOwnDevice))
-		return
-	}
-
-	var deviceInfo *api.DeviceCreateInfo
-	err := c.ShouldBind(&deviceInfo)
-	if err != nil {
-		c.Error(errors.New(err))
-		return
-	}
-
-	device.Name = deviceInfo.Name
-	aerr = h.deviceRepo.Update(device)
-	if aerr != nil {
-		c.Error(aerr)
-		return
-	}
-
-	c.JSON(http.StatusOK, api.DeviceInfo{
-		ID:     device.ID,
-		Name:   device.Name,
-		Code:   device.Code,
-		Secret: device.Secret,
-	})
-}
-
-func (h *DevicesHandler) deleteDevice(c *gin.Context) {
-	deviceId := c.Param(IdPathParam)
-	device, aerr := h.deviceRepo.GetById(deviceId)
-	if aerr != nil {
-		c.Error(aerr)
-		return
-	}
-
-	ownerId := middleware.GetUserIdFromContext(c)
-	if ownerId != device.UserID {
-		c.Error(errors.New(UserDoesNotOwnDevice))
-		return
-	}
-
-	aerr = h.deviceRepo.Delete(device)
-	if aerr != nil {
-		c.Error(aerr)
-		return
-	}
-
 	c.Status(http.StatusNoContent)
 }
--- src/controller/users.go
+++ src/controller/users.go
@@ -3,9 +3,16 @@ package controller
 import (
 	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
+	"github.com/go-errors/errors"
+	"github.com/google/uuid"
+	"github.com/pc-power-api/src/api"
 	"github.com/pc-power-api/src/controller/gateway"
 	"github.com/pc-power-api/src/controller/middleware"
+	"github.com/pc-power-api/src/infra/entity"
 	"github.com/pc-power-api/src/infra/repo"
+	"github.com/pc-power-api/src/pubsub"
+	"github.com/pc-power-api/src/util"
+	"net/http"
 )
 
 type UsersHandler struct {
@@ -22,6 +29,14 @@ func NewUsersHandler(e *gin.Engine, jwtMiddleware *jwt.GinJWTMiddleware, userRep
 	group := e.Group("/user", jwtMiddleware.MiddlewareFunc())
 	{
 		group.GET("/gateway", handler.gateway)
+
+		deviceGroup := group.Group("/devices")
+
+		deviceGroup.POST("/", handler.createDevice)
+		deviceGroup.GET("/", handler.getDevices)
+		deviceGroup.GET("/:"+IdPathParam, handler.getDevice)
+		deviceGroup.PUT("/:"+IdPathParam, handler.updateDevice)
+		deviceGroup.DELETE("/:"+IdPathParam, handler.deleteDevice)
 	}
 }
 
@@ -33,4 +48,166 @@ func (h *UsersHandler) gateway(c *gin.Context) {
 	}
 
 	gateway.NewUserClient(c.Writer, c.Request, user)
+}
+
+func (h *UsersHandler) getDevices(c *gin.Context) {
+	userId := middleware.GetUserIdFromContext(c)
+	user, aerr := h.userRepo.GetById(userId)
+	if aerr != nil {
+		c.Error(aerr)
+		return
+	}
+
+	devicesInfoList := api.DeviceInfoList{
+		OnlineDevices:  make([]api.DeviceInfo, 0),
+		OfflineDevices: make([]api.DeviceInfo, 0),
+	}
+	for _, device := range user.Devices {
+		if conn, ok := gateway.ConnectedDevices[device.ID]; ok {
+			devicesInfoList.OnlineDevices = append(devicesInfoList.OnlineDevices, api.DeviceInfo{
+				ID:     device.ID,
+				Name:   device.Name,
+				Code:   device.Code,
+				Secret: device.Secret,
+				Status: conn.GetStatus(),
+				Online: true,
+			})
+		} else {
+			devicesInfoList.OfflineDevices = append(devicesInfoList.OfflineDevices, api.DeviceInfo{
+				ID:     device.ID,
+				Name:   device.Name,
+				Code:   device.Code,
+				Secret: device.Secret,
+				Status: 0,
+				Online: false,
+			})
+		}
+	}
+
+	c.JSON(http.StatusOK, devicesInfoList)
+}
+
+func (h *UsersHandler) getDevice(c *gin.Context) {
+	deviceId := c.Param(IdPathParam)
+	device, aerr := h.deviceRepo.GetById(deviceId)
+	if aerr != nil {
+		c.Error(aerr)
+		return
+	}
+
+	ownerId := middleware.GetUserIdFromContext(c)
+	if ownerId != device.UserID {
+		c.Error(errors.New(UserDoesNotOwnDevice))
+		return
+	}
+
+	status := 0
+	online := false
+	if conn, ok := gateway.ConnectedDevices[device.ID]; ok {
+		status = conn.GetStatus()
+		online = true
+	}
+	c.JSON(http.StatusOK, api.DeviceInfo{
+		ID:     device.ID,
+		Name:   device.Name,
+		Code:   device.Code,
+		Secret: device.Secret,
+		Status: status,
+		Online: online,
+	})
+}
+
+func (h *UsersHandler) createDevice(c *gin.Context) {
+	var deviceInfo *api.DeviceCreateInfo
+	err := c.ShouldBind(&deviceInfo)
+	if err != nil {
+		c.Error(errors.New(err))
+		return
+	}
+
+	ownerId := middleware.GetUserIdFromContext(c)
+	deviceUuid := uuid.New()
+	deviceCode := util.GenerateRandomString(DeviceCodeLength)
+	deviceSecret := util.GenerateRandomString(DeviceSecretLength)
+
+	var device = entity.Device{
+		ID:     deviceUuid.String(),
+		Name:   deviceInfo.Name,
+		Code:   deviceCode,
+		Secret: deviceSecret,
+		UserID: ownerId,
+	}
+
+	aerr := h.deviceRepo.Create(&device)
+	if aerr != nil {
+		c.Error(aerr)
+		return
+	}
+	pubsub.Publish(ownerId, device)
+
+	c.JSON(http.StatusOK, api.DeviceInfo{
+		ID:     device.ID,
+		Name:   device.Name,
+		Code:   device.Code,
+		Secret: device.Secret,
+	})
+}
+
+func (h *UsersHandler) updateDevice(c *gin.Context) {
+	deviceId := c.Param(IdPathParam)
+	device, aerr := h.deviceRepo.GetById(deviceId)
+	if aerr != nil {
+		c.Error(aerr)
+		return
+	}
+
+	ownerId := middleware.GetUserIdFromContext(c)
+	if ownerId != device.UserID {
+		c.Error(errors.New(UserDoesNotOwnDevice))
+		return
+	}
+
+	var deviceInfo *api.DeviceCreateInfo
+	err := c.ShouldBind(&deviceInfo)
+	if err != nil {
+		c.Error(errors.New(err))
+		return
+	}
+
+	device.Name = deviceInfo.Name
+	aerr = h.deviceRepo.Update(device)
+	if aerr != nil {
+		c.Error(aerr)
+		return
+	}
+
+	c.JSON(http.StatusOK, api.DeviceInfo{
+		ID:     device.ID,
+		Name:   device.Name,
+		Code:   device.Code,
+		Secret: device.Secret,
+	})
+}
+
+func (h *UsersHandler) deleteDevice(c *gin.Context) {
+	deviceId := c.Param(IdPathParam)
+	device, aerr := h.deviceRepo.GetById(deviceId)
+	if aerr != nil {
+		c.Error(aerr)
+		return
+	}
+
+	ownerId := middleware.GetUserIdFromContext(c)
+	if ownerId != device.UserID {
+		c.Error(errors.New(UserDoesNotOwnDevice))
+		return
+	}
+
+	aerr = h.deviceRepo.Delete(device)
+	if aerr != nil {
+		c.Error(aerr)
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
